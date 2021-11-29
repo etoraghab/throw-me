b8aade705da82e2ce2850bd659779b69c2e92e18
volcareso <volcareso@gmail.com>
2021-11-29 19:50:50 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
-
+import { extendTheme } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
+
+ const config = {
+   initialColorMode: 'light',
+   useSystemColorMode: false,
+ }
+
   return (
     <ChakraProvider>  
       <Component {...pageProps} />
