b083429135492c7588b75a985403ad23d864438e
Max Programming <ui.sabuwala@gmail.com>
2022-01-25 14:21:45 +0530

    ⚡️ Use SSR instead of fetching and redirecting on frontend

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,50 +1,33 @@
-import React from 'react'
-import fetch from 'node-fetch'
-import { useRouter } from 'next/router'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Button, Link } from "@chakra-ui/react"
-import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { Input } from "@chakra-ui/react"
-import { Heading, Text } from '@chakra-ui/react'
-import Script from 'next/script'
+import React from 'react';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({ log: ['query', 'info'] });
+
+/** @type {import('next').GetServerSideProps} */
+export const getServerSideProps = async context => {
+  const { pid } = context.query;
+  if (pid.length <= 20 && pid.length >= 3) {
+    const link = await prisma.urls.findUnique({
+      where: {
+        name: pid,
+      },
+    });
+    if (!link)
+      return {
+        notFound: true,
+      };
+    return {
+      redirect: {
+        permanent: false,
+        destination: link.url,
+      },
+    };
+  }
+  return {
+    notFound: true,
+  };
+};
 
 export default function To() {
-    const router = useRouter();
-    const { pid } = router.query;
-    let todo = {
-        name: pid
-    };
-
-    fetch('/api/get/', {
-        method: 'POST',
-        body: JSON.stringify(todo),
-        headers: { 'Content-Type': 'application/json' }
-    }).then(res => res.json())
-        .then(json => window.location.replace(json.url))
-
-    return (<>
-<div className={styles.container}>
-      <Head>
-        <title>please wait...</title>
-        <meta name="description" content="please wait ......" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <Heading color="teal">Please wait for ~5 seconds...</Heading>
-        <Text fontSize='md'>
-           Chill 🧊 <br />
-             you&apos;ll be surely redirected if the link entered was correct!
-        </Text>
-      </main>
-
-      <footer className={styles.footer}>
-        <span className={styles.logo}>
-          <Link color="teal.500" href="https://volcareso.js.org/">(c) volcareso</Link>
-          <Link color="teal.500" href="https://twitter.com/volcareso">Twitter</Link>
-          <Link color="teal.500" href="https://github.com/volcareso">GitHub</Link>
-        </span>
-      </footer>
-    </div>
-    </>)
+  return <></>;
 }
