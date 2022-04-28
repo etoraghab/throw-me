c95f646b2f2fb6f6518899a203e0aa2a99124d30
atordvairn <atordvairn@gmail.com>
2022-04-28 10:38:36 +0000

    phishing protection

--- pages/api/create/index.js
+++ pages/api/create/index.js
@@ -2,6 +2,9 @@ import { PrismaClient } from '@prisma/client';
 import { nanoid } from 'nanoid';
 const prisma = new PrismaClient({ log: ['query', 'info'] });
 import rateLimit from '../../../utils/rate-limit';
+import { encode } from "url-encode-decode"
+import fetch, { BodyMixin } from 'node-fetch';
+import { findPackageData } from '@babel/core/lib/config/files';
 
 const limiter = rateLimit({
   interval: 60 * 1000,
@@ -18,49 +21,62 @@ export default async function handler(req, res) {
     } else {
       async function main() {
         const { name, url } = req.body;
-        // Id "url" is not correct, return and respond with failed
-        if (!url || url.trim() === '') {
-          return res
-            .status(400)
-            .send({ message: 'failed', reason: 'parameters' });
-        }
-        // If "name" isn't in body, generate a new one
-        if (!name) {
-          const randomName = nanoid(5);
-          await prisma.urls.create({
-            data: {
-              url,
-              name: randomName,
-            },
-          });
-          return res.status(200).send({ message: 'success', name: randomName });
-        }
-        // Check "name"'s characters and create with a custom name
-        if (name.length <= 20 && name.length >= 3) {
-          try {
-            await prisma.urls.create({
-              data: {
-                url,
-                name,
-              },
-            });
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).send({ message: 'success', name });
-          } catch (e) {
-            if (e.code == 'P2002') {
-              res.setHeader('Content-Type', 'application/json');
-              res.status(400).send({
-                message: 'The Name Already Exists! Choose A Better Name!',
+        const key = process.env.ENV_KEY;
+
+        await fetch("https://ipqualityscore.com/api/json/url/" + key + "/" + encode(url)).then(async (src) => {
+          let maindata;
+          await src.json().then((data) => {
+            maindata = data
+          })
+          if (!(maindata.risk_score >= 60)) {
+            // Id "url" is not correct, return and respond with failed
+            if (!url || url.trim() === '') {
+              return res
+                .status(400)
+                .send({ message: 'failed', reason: 'parameters' });
+            }
+            // If "name" isn't in body, generate a new one
+            if (!name) {
+              const randomName = nanoid(5);
+              await prisma.urls.create({
+                data: {
+                  url,
+                  name: randomName,
+                },
               });
+              return res.status(200).send({ message: 'success', name: randomName });
+            }
+            // Check "name"'s characters and create with a custom name
+            if (name.length <= 20 && name.length >= 3) {
+              try {
+                await prisma.urls.create({
+                  data: {
+                    url,
+                    name,
+                  },
+                });
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).send({ message: 'success', name });
+              } catch (e) {
+                if (e.code == 'P2002') {
+                  res.setHeader('Content-Type', 'application/json');
+                  res.status(400).send({
+                    message: 'The Name Already Exists! Choose A Better Name!',
+                  });
+                } else {
+                  res.setHeader('Content-Type', 'application/json');
+                  res.status(400).send({ message: e.code });
+                }
+              }
             } else {
               res.setHeader('Content-Type', 'application/json');
-              res.status(400).send({ message: e.code });
+              res.status(400).send({ message: 'failed', reason: 'parameters' });
             }
+          } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400).send({ message: 'phishing site!', reason: 'phish' });
           }
-        } else {
-          res.setHeader('Content-Type', 'application/json');
-          res.status(400).send({ message: 'failed', reason: 'parameters' });
-        }
+        })
       }
 
       main()
